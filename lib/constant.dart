b86657f419f76a1d2e5c53a0a466efc354e75844
astarikafanny <afanny.67@gmail.com>
2021-01-20 13:16:31 +0700

    Android-Aplikasi-Perhitungan-Suara

---
+++ lib/constant.dart
@@ -0,0 +1,3 @@
+// local emulator: http://192.168.43.102:8000
+// heroku: http://laravel-pps.herokuapp.com
+const String API_ENDPOINT = "http://laravel-pps.herokuapp.com";
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:pps/screen/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aplikasi Proses Perhitungan Surat Suara',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
